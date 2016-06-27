11bfca223bd889f06d828f31ad97c876549a592b
Jimmy Song <jingchao.song@tendcloud.com>
2016-06-27 19:08:23 +0800

    add yarn-env.sh

--- hadoop-conf/withoutha/hadoop-env.sh
+++ hadoop-conf/withoutha/hadoop-env.sh
@@ -42,7 +42,7 @@ for f in $HADOOP_HOME/contrib/capacity-scheduler/*.jar; do
 done
 
 # The maximum amount of heap to use, in MB. Default is 1000.
-#export HADOOP_HEAPSIZE=
+export HADOOP_HEAPSIZE=2048m
 #export HADOOP_NAMENODE_INIT_HEAPSIZE=""
 
 # Extra Java runtime options.  Empty by default.
